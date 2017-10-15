e5ea292c38c26a7a8f388373ea7013b6c4bd6585
liujunling03 <liujunling03@baidu.com>
2017-10-15 20:01:24 +0800

    add template support

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"net/http"
 )
 
 var DB = make(map[string]string)
 
 func main() {
 
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test?parseTime=true")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?parseTime=true")
 	defer db.Close()
 	if err != nil{
 		log.Fatalln(err)
@@ -23,44 +24,45 @@ func main() {
 	db.SetMaxOpenConns(20)
 
 	if err := db.Ping(); err != nil{
-		log.Fatalln("error")
+		log.Fatalln(err)
 	}
+
+	vote := "/vote";
 
 	app := gin.Default()
 	app.Use(favicon.New("./favicon.ico"))
 
-	app.GET("/ping", func(c *gin.Context) {
+	app.Static("/assets", "./assets")
+
+	app.GET(vote + "/", func(c *gin.Context) {
 		c.String(200, "Hello favicon.")
 	})
+	app.LoadHTMLGlob("templates/*")
+	app.GET(vote + "/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "vote.html", gin.H{
+			"title": "投票系统",
+		})
+	})
 
-	app.POST("/login", func(c *gin.Context) {
+	app.POST(vote + "/login", func(c *gin.Context) {
 
 	})
 
-	app.POST("/counter/competitor/score", func(c *gin.Context) {
+	app.POST(vote + "/competitor/score", func(c *gin.Context) {
 
 	})
 
-	app.POST("/counter/team/score", func(c *gin.Context) {
+	app.POST(vote + "/team/score", func(c *gin.Context) {
 
 	})
 
-	app.GET("/counter/competitors", func(c *gin.Context) {
+	app.GET(vote + "/competitors", func(c *gin.Context) {
 
 	})
 
-	app.GET("/counter/judgers", func(c *gin.Context) {
+	app.GET(vote + "/judgers", func(c *gin.Context) {
 
 	})
-
-	app.GET("/counter/team/score", func(c *gin.Context) {
-
-	})
-
-	app.GET("/counter/competitor/score", func(c *gin.Context) {
-
-	})
-
 
 	app.Run(":8080")
 }
